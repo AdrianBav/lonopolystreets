4c152741ae91f881ae49865e335538cac6250ab2
Adrian Bavister <adrian@bavanco.co.uk>
2019-08-18 23:21:22 -0500

    Added tailwind custom forms

--- public/js/app.js
+++ public/js/app.js
@@ -14209,12 +14209,6 @@ module.exports = g;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-///require('./bootstrap');
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
 /**
  * The following block of code may be used to automatically register your
